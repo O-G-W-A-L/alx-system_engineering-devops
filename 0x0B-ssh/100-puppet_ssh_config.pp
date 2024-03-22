d8169b1049288394bc9e47661eccfe11827fc40a
O-G-W-A-L <ogwal04@gmail.com>
2024-03-22 06:26:26 -0700

    client config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# edit config file
+
+file_line { 'Turn off passwd':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'identity change':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
