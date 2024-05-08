281855b35f4ff83e3e25b3f8d7b3183a8f6ef639
O-G-W-A-L <ogwalja04@gmail.com>
2024-05-08 00:32:01 -0700

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""get the number of subs"""
+"""get a lit of suscribber on a subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Get the number of subs to a subreddit"""
-    url = "https://www.reddit.com/r/{sub}/about.json"
-    headers = {
-            "User-Agent": "linux:0x16.api.advanced",
-    }
-    res = requests.get(url=url.format(sub=subreddit), headers=headers)
-    if res.status_code != 200:
-        return 0
-    data = res.json()["data"]
-    return data.get("subscribers", 0)
+    """number of suscribers"""
+    if subreddit is None or type(subreddit) != str:
+        return (0)
+    url = "http://www.reddit.com/r/{}/about.json".format(
+            subreddit)
+    headers = {'User-Agent': 'API project'}
+    response = requests.get(url, headers=headers)
+    if (response.status_code != 200):
+        return (0)
+    sus = response.json().get("data").get('subscribers', 0)
+    return (sus)
